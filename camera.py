b3014983a5c09fbc3e72bd80c8410dae314930da
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2022-03-04 13:16:01 +0100

    feat: implemented camera image capturing as a generator

--- camera.py
+++ camera.py
@@ -1,30 +1,59 @@
 from random import randrange
 from time import sleep
+from typing import Any, AsyncIterable, Iterable, Tuple
 from image import write_to_file
 import pyrealsense2 as rs
 from realsense.realsense_device_manager import DeviceManager
+from functools import lru_cache
 
 
-if __name__ == "__main__":
+@lru_cache(1)
+def get_device_manager() -> DeviceManager:
+    print("initializing realsense device manager")
+    config = rs.config()
+    config.enable_stream(rs.stream.infrared, 1, 1280, 800, rs.format.y8, 30)
+    device_manager = DeviceManager(rs.context(), config)
+    device_manager.enable_all_devices()
+    return device_manager
+
+
+def poll_frame_data() -> Iterable[Tuple[int, Any]]:
     device_manager = None
     try:
-      config = rs.config()
-      config.enable_stream(rs.stream.infrared, 1, 1280, 800, rs.format.y8, 30)
-      device_manager = DeviceManager(rs.context(), config)
-      device_manager.enable_all_devices()
-      frames = []
-      for k in range(5):
-          frames = device_manager.poll_frames()
-          print(frames)
-          frame_id = randrange(1000)
-          for camera_id in frames:
-            frame = frames[camera_id]
-            frame_value = list(frame.values())[0]
-            frame_data = frame_value.get_data()
-            print( frame_data )
-            write_to_file(frame_data, f'output_{frame_id}_cid_{camera_id[0][-3:]}.png')
-          print("Done")
-          sleep(10)
+        device_manager = get_device_manager()
+        frames = []
+        while True:
+            frames = device_manager.poll_frames()
+            for camera_id in frames:
+                frame = frames[camera_id]
+                frame_value = list(frame.values())[0]
+                frame_data = frame_value.get_data()
+                short_camera_id = camera_id[0][-3:]
+                yield short_camera_id, frame_data
     finally:
-      if (device_manager):
-        device_manager.disable_streams()
+        if device_manager:
+            device_manager.disable_streams()
+
+
+def write_images():
+    device_manager = None
+    try:
+        device_manager = get_device_manager()
+        frames = []
+        for k in range(5):
+            frames = device_manager.poll_frames()
+            print(frames)
+            frame_id = randrange(1000)
+            for camera_id in frames:
+                frame = frames[camera_id]
+                frame_value = list(frame.values())[0]
+                frame_data = frame_value.get_data()
+                print(frame_data)
+                write_to_file(
+                    frame_data, f"output_{frame_id}_cid_{camera_id[0][-3:]}.png"
+                )
+            print("Done")
+            sleep(10)
+    finally:
+        if device_manager:
+            device_manager.disable_streams()
