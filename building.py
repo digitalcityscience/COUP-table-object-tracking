95ccb6f6a7de166677c9d26213b725374a6b994b
John Tableman <john.tableman@digitalcityscience.rocks>
2022-03-09 10:09:58 +0100

    chore: update types

--- building.py
+++ building.py
@@ -1,17 +1,19 @@
+import time
 from dataclasses import dataclass
 from typing import Dict, List, Union
 
 from detection import Corner, normalizeCorners
 
 Position = List[Union[float, int]]
+CameraId = Union[int, str]
 
 
 @dataclass
 class Building:
     id: int
     position: Position
-    lastSeen: int
-    cameraId: str
+    lastSeen: float
+    cameraId: CameraId
     confidence: int = 0
 
     def updateConfidence(self, currentLoop):
@@ -36,7 +38,7 @@ BuildingDictionary = Dict[int, Building]
 
 def add_detected_buildings_to_dict(
     ids: List[int],
-    cameraId: str,
+    cameraId: CameraId,
     corners: List[Corner],
     loopcount: int,
     buildingDict: BuildingDictionary,
@@ -57,6 +59,26 @@ def add_detected_buildings_to_dict(
                     )
                 else:
                     buildingDict[markerID].updatePosition(position, loopcount)
+
+
+def map_detected_buildings(
+    cameraId: CameraId, ids: List[int], corners: List[Corner]
+) -> Dict[int, Building]:
+    buildingDict: Dict[int, Building] = {}
+    if ids is not None:
+        for i in range(0, len(ids)):
+            markerID = int(ids[i])
+            now = time.time()
+
+            if markerID is not 500:
+                position = normalizeCorners(corners[i])
+                buildingDict[markerID] = Building(
+                    id=markerID,
+                    position=position,
+                    lastSeen=now,
+                    cameraId=cameraId,
+                )
+    return buildingDict
 
 
 def discard_low_confidence_buildings(
