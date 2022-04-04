2ce0ca4e52b41490605dc3de8bddc4cf84790df6
Dobroslav Totev <dobroslav.totev@hcu-hamburg.de>
2022-04-04 12:23:13 +0200

    chore: update test_printJSON with better mock data

--- test/test_building.py
+++ test/test_building.py
@@ -1,6 +1,6 @@
+import time
 from building import Building, printJSON
 
-
 def test_printJSON():
-    buildingsDict = {19: Building(19, [10, 11], 1111, "cameraId1")}
-    assert printJSON(buildingsDict) == {19: [10, 11]}
+    buildingsDict = {19: Building(19, [449, 614, 78.90624111411], time.time(), "cameraId1")}
+    assert printJSON(buildingsDict) == {19: [449, 614, 78.90624111411]}
